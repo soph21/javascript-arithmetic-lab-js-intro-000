5b4c560c7e5d8c0a6178752b0e67f358f73bdaba
Sophie Shin <sophie@alloy.co>
2021-02-08 21:42:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function decrement(n) {
   return n-=1
 }
 function makeInt(n) {
-  parseInt('n',10)
+  return parseInt('n', 10)
 }
 function preserveDecimal(n) {
-  parseFloat('n')
+  return parseFloat('n')
 }
